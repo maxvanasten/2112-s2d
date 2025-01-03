9a2babbfc77377325e1354c80c9591561b98fda5
Max van Asten <maxvasten@gmail.com>
2025-01-03 20:51:13 +0100

    Added boilerplate code

---
+++ client/main.js
@@ -0,0 +1,8 @@
+import { Core } from "./s2d_engine/core.js";
+import Game from "./space_game/main.js";
+
+const core = new Core();
+
+core._import_game(Game);
+core._connect_canvas("game_canvas", true);
+core._start_game();
